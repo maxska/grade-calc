7baddfbc81a0dd05fde292d53a61b60a28c59db8
Max <maxskan98@gmail.com>
2020-10-27 13:10:20 +0100

    Window works now

--- src/gradeCalc/GUI.java
+++ src/gradeCalc/GUI.java
@@ -1,6 +1,43 @@
 package gradeCalc;
 
-public class GUI 
-{
+import javax.swing.JFrame;
 
+public class GUI extends JFrame
+{
+	private static final int FRAME_WIDTH  = 400;
+	private static final int FRAME_HEIGHT = 600;    
+	
+	
+	public static void main(String[] args)
+	{
+		GUI gui = new GUI();
+	}
+	
+	
+	/*
+	 * Constructor
+	 */
+	public GUI()
+	{
+		//intitializeMembers();
+		createComponents();
+		
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		setLocation(200, 200);
+		setTitle("gradeCalc");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);		
+	}
+	
+	
+	private void initializeMembers()
+	{
+		
+	}
+	
+	
+	private void createComponents()
+	{
+		
+	}
 }
